8c3f951f2b4018f7b952e48e81a7f0b174ed535b
StepanTWL <h3xp3ctb@gmail.com>
2023-07-31 21:36:50 +0300

    31.07.2023

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 import random
+from IPython.display import clear_output
+
+count_type1 = 0
+count_type2 = 0
+count_type3 = 0
+count_axle = 0
+count = 0
 
 while True:
-	signal1 = random.randint(0,1)
-	signal2 = random.randint(0,1)
+	count += 1
+	signal1 = random.randint(0, 1)
+	signal2 = random.randint(0, 1)
+
+	if signal1 == 0 and signal2 == 1:
+		count_type1 += 1
+	elif signal1 == 0 and signal2 == 0 and count_type1 > 0:
+		count_type2 += 1
+	elif signal1 == 1 and signal2 == 0 and count_type2 > 0:
+		count_type3 += 1
+	elif count_type1 > 5 and count_type2 > 4 and count_type3 > 5:
+		count_type1 = 0
+		count_type2 = 0
+		count_type3 = 0
+		count_axle += 1
+		clear_output()
+		print(count, count_axle)
+
+
+
 
